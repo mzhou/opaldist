b10bce402510af838c4bdace0cc5d1dd5151304e
Michael Zhou <mzhou@cse.unsw.edu.au>
2014-02-25 21:57:21 +1100

    Add Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,1 +1,11 @@
-main = putStrLn "Hello, World!"
+import System.Environment (getArgs)
+
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.Graph.Inductive.Query.SP (spLength)
+
+import Opal.Distance.Stations (Station)
+import Opal.Distance.Tracks (graph)
+
+main = do
+    (orig:dest:_) <- getArgs
+    putStrLn $ show $ spLength (fromEnum $ ((read orig) :: Station)) (fromEnum $ ((read dest) :: Station)) (graph :: Gr Station Int)
