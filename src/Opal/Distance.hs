221646e86da5e995c60963ba3394db2934e58346
Michael Zhou <mzhou@cse.unsw.edu.au>
2014-03-08 09:29:18 +1100

    Export Units

--- src/Opal/Distance.hs
+++ src/Opal/Distance.hs
@@ -1,7 +1,9 @@
 module Opal.Distance (
     module Opal.Distance.Calc,
     module Opal.Distance.Stations,
+    module Opal.Distance.Units,
 ) where
 
 import Opal.Distance.Calc
 import Opal.Distance.Stations
+import Opal.Distance.Units
